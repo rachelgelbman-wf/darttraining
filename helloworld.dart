60d752ca9aecc65816821493dee69a1f321e0304
Rachel Gelbman <rachel.gelbman@webfilings.com>
2018-11-12 10:00:46 -0700

    Add Line Graphic

--- helloworld.dart
+++ helloworld.dart
@@ -6,12 +6,19 @@ String sunshinemessage = "Good morning sunshine";
 void main () {
   int count = 0;
   while (true) {
-    print ("$count I'm feeling loopy");
     count = count +1;
-    print (count); 
-  } 
+    String nextLine = getNextLine(count);
+    print (nextLine);
+      } 
 }
 
-String getNextLine () {String getNextLine = "";
-
+String getNextLine (int count) {
+  String getLine = "_ _ _ _ _ _ _ _ _ _";
+  int barLocation = count % 10;
+  String newLine = replaceCharAt(getLine, barLocation, "|");
+  return newLine;
+ 
+}
+String replaceCharAt(String oldString, int index, String newChar) {
+  return oldString.substring(0, index) + newChar + oldString.substring(index + 1);
 }

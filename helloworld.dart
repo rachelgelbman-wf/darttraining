29b59c3f89220ba3fc73dfd83c24d88985fabb73
Rachel Gelbman <rachel.gelbman@webfilings.com>
2018-11-13 09:20:44 -0700

    For Loops

--- helloworld.dart
+++ helloworld.dart
@@ -1,17 +1,23 @@
+
 String getString () {String irishmessage =  "Top of the Mornin to ya";
 String sunshinemessage = "Good morning sunshine";
  String fullmessage =  irishmessage + " and " + sunshinemessage;
  return fullmessage;
 }
 void main () {
-  int count = 0;
-  while (true) {
+    List <String> listWords= ["Elephant", "Giraffe", "Hippo"];
+  for (int i=0; i<listWords.length; i=i+1) 
+   { 
+     print (listWords [i]);
+        int count = 0;
+  for (int i=0; i<30; i=i+1) {
     count = count +1;
+    print (count);
     String nextLine = getNextLine(count);
     print (nextLine);
       } 
+   }
 }
-
 String getNextLine (int count) {
   String getLine = "_ _ _ _ _ _ _ _ _ _";
   int barLocation = count % 10;
@@ -22,3 +28,4 @@ String getNextLine (int count) {
 String replaceCharAt(String oldString, int index, String newChar) {
   return oldString.substring(0, index) + newChar + oldString.substring(index + 1);
 }
+
